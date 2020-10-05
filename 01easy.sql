3915015562d209024dec23485e797f48c318a38e
wangchan <dacn2017@gmail.com>
2020-10-05 18:05:45 +0800

    solve three easy probs

--- 01easy.sql
+++ 01easy.sql
@@ -11,3 +11,22 @@ select
             Employee
         Order BY Salary DESC
         LIMIT 1 OFFSET 1) AS SecondHighestSalary
+
+--#181. Employees Earning More Than Their Managers
+select a.Name AS "Employee"
+FROM Employee AS a, Employee AS b
+where a.ManagerId=b.Id	 and a.salary>=b.salary;
+
+--#182. Duplicate Emails
+Select Distinct a.Email 
+
+--#183. Customers Who Never Order
+select customers.name as 'Customers'
+from customers
+where customers.id not in
+(
+    select customerid from orders
+);
+
+From Person AS a,Person AS b
+where a.Id != b.Id and a.Email = b.Email
